50cac369b7c208cfadf5dfec609455ca40993bf9
jtamsut <jtamsut1993@gmail.com>
2018-06-15 15:02:05 -0700

    update change

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,20 @@
-# Uses python3
-import sys
+#python3
 
 def get_change(m):
-    #write your code here
-    return m
+    coins = 0 
+    while (m > 0):
+        if m >= 10:
+            coins+=1
+            m-=10
+        elif m >= 5:
+            coins+=1
+            m-=5
+        elif m >=1:
+            coins+=1
+            m-=1
+    return coins
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
