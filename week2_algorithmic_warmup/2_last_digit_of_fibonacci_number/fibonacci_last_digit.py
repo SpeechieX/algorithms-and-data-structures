2885fc71eb1b8e2e27afbe876e8c5a686a300d84
jtamsut <jtamsut1993@gmail.com>
2018-06-16 16:32:07 -0700

    complete fibonacci last digit

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,6 +1,4 @@
 # Uses python3
-import sys
-
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
         return n
@@ -9,11 +7,11 @@ def get_fibonacci_last_digit_naive(n):
     current  = 1
 
     for _ in range(n - 1):
-        previous, current = current, previous + current
+        previous, current = (current % 10), ((previous + current)%10)
+        print(current)
 
     return current % 10
 
-
-    input = sys.stdin.read()
-    n = int(input)
+if __name__ == '__main__':
+    n = int(input())
     print(get_fibonacci_last_digit_naive(n))
