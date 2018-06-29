c7c4f48e805704f2468805b294c74a14296cf964
jtamsut <jtamsut1993@gmail.com>
2018-06-28 18:38:21 -0700

    restructure repo

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Algorithms and Data Structures
 
 This repository contains implementations of various data structures and algorithms. My goal here is to focus on algorithmic paradigms and data structures.
+
+## What is the Difference Between Dynamic Programming and Divide-And-Conquer?
+
+Break a problem into sub-problems. 
+
+Do they overlap? 
+
+How can you re-combine the sub-problems into an overall solution.
+
+Memoization 
+Tabulation 
+
+Edit distance problem
--- interview-cake/merge_meeting_times.c
+++ interview-cake/merge_meeting_times.c
@@ -1,39 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 
-#define MAX_OUTPUT_LENGTH 100
+// #define MAX_OUTPUT_LENGTH 100
 
-typedef struct {
-    unsigned int startTime;
-    unsigned int endTime;
-} Meeting;
+// typedef struct {
+//     unsigned int startTime;
+//     unsigned int endTime;
+// } Meeting;
 
-size_t mergeRanges(const Meeting *input, size_t inputLength, Meeting *output, size_t maxOutputLength) {
-    for (int i=0; i<inputLength; i++) {
-        if (input[i].endTime >= input[i+1].startTime) {
+// size_t mergeRanges(const Meeting *input, size_t inputLength, Meeting *output, size_t maxOutputLength) {
+//     for (int i=0; i<inputLength; i++) {
+//         if (input[i].endTime >= input[i+1].startTime) {
 
-            struct Meeting new_meeting = malloc(sizeof(Meeting));
-            new_meeting.startTime = input[i].startTime;
-            new_meeting.endTime = input[i+1].endTime;
+//             struct Meeting new_meeting = malloc(sizeof(Meeting));
+//             new_meeting.startTime = input[i].startTime;
+//             new_meeting.endTime = input[i+1].endTime;
 
-        }
-        printf("start: %d | and end: %d\n", new_meeting.startTime, new_meeting.endTime);
+//         }
+//         printf("start: %d | and end: %d\n", new_meeting.startTime, new_meeting.endTime);
         
-    }
+//     }
     
 
-    return 1;
-}
+//     return 1;
+// }
 
-int main() {
-    const Meeting meetings[] = { {1, 3}, {2, 4} };
-    const Meeting expected[] = { {1, 4} };
+// int main() {
+//     const Meeting meetings[] = { {1, 3}, {2, 4} };
+//     const Meeting expected[] = { {1, 4} };
 
-    const Meeting *o[MAX_OUTPUT_LENGTH];
+//     const Meeting *o[MAX_OUTPUT_LENGTH];
     
 
-    mergeRanges(meetings, 2, o, MAX_OUTPUT_LENGTH);
+//     mergeRanges(meetings, 2, o, MAX_OUTPUT_LENGTH);
 
-    return 0;
-}
+//     return 0;
+// }
