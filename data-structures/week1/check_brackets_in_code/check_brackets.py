f28717a93acd8cce50027e39b4a0dfb3e84b7e3f
jtamsut <jtamsut1993@gmail.com>
2018-07-11 15:14:29 -0700

    implement balanced brackets

--- data-structures/week1/check_brackets_in_code/check_brackets.py
+++ data-structures/week1/check_brackets_in_code/check_brackets.py
@@ -24,7 +24,7 @@ class Stack:
         self.stack.append([item, idx])
 
     def pop(self):
-        if len(self.stack) > 0: 
+        if not self.empty(): 
             return self.stack.pop()
         else: 
             return False
@@ -34,9 +34,9 @@ class Stack:
             return True 
         return False
 
-def isBalanced(text): 
+def isBalanced(text):
     stack = Stack()
-    valid = True
+    
     for i, next in enumerate(text):
 
         if next == '(' or next == '[' or next == '{':
@@ -44,63 +44,25 @@ def isBalanced(text):
             pass
 
         if next == ')' or next == ']' or next == '}':
+            
+            if stack.empty(): 
+                return i + 1
+            
             # Caring top of stack and current character
             top_stack = stack.pop()
+            
             curr_bracket = Bracket(top_stack[0], i)
 
-            if top_stack == False: 
-                return i
-
             if not curr_bracket.Match(next):
-                return top_stack[1]
+                return i + 1
             
             pass
     
+    if not stack.empty():
+        return stack.pop()[1] + 1
+
     return "Success"
 
-#-----------Testing below
 
-from os import listdir
-from os.path import isfile, join
-
-# Provide relative path below
-mypath = "./tests"
-
-onlyfiles = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
-
-for i in range(0, len(onlyfiles) - 1, 2): 
-    input_file = open("./tests/" + onlyfiles[i], "r")
-    output_file = open("./tests/" + onlyfiles[i+1], "r")
-    print('Input: ', input_file.read())
-    print(isBalanced(input_file.read()))
-
-# if valid: 
-#     print("Success")
-
-# if __name__ == "__main__":
-#     text = input()
-#     stack = Stack()
-#     valid = True
-    
-#     for i, next in enumerate(text):
-        
-#         if next == '(' or next == '[' or next == '{':
-#             stack.push(next, i)
-#             pass
-
-#         if next == ')' or next == ']' or next == '}':
-#             # Caring top of stack and current character
-#             top_stack = stack.pop()
-#             curr_bracket = Bracket(top_stack[0], i)
-        
-#             # if top_stack == False: 
-#             #     print()
-            
-#             if not curr_bracket.Match(next):
-#                 valid = False
-#                 print(top_stack[1])
-#                 break
-#             pass
-
-# if valid: 
-#     print("Success")
+if __name__ == "__main__":
+    print(isBalanced(input()))
