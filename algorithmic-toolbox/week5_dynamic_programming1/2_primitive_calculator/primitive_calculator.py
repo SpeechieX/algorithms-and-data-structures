d64142a085581380b73a0022412445e1b4f6d887
jtamsut <jtamsut1993@gmail.com>
2018-07-21 12:43:25 -0700

    complete primitive calculator problem

--- algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
+++ algorithmic-toolbox/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.py
@@ -2,45 +2,51 @@
 import sys
 import math 
 
-# Utitlity functions 
-
-mult2 = lambda x: 2 * x
-mult3 = lambda x: 3 * x
-add1 = lambda x: x + 1
-
-# =============================================
-
 def optimal_sequence(n):
     
-    ops = { 
-        "add1": add1, 
-        "mult3": mult3, 
-        "mult2": mult2 
-    }
+    # [value, min number of ops, prev index to get here]
+    ops_count = [ [i+1,math.inf, 1] if i != 0 else [1,0, 0] for i in range(0, n) ]
 
-    opsCount = [ math.inf if i != 0 else 0 for i in range(0, n) ]
+    for i in range(1, len(ops_count)):
+        cur_val = ops_count[i][0]
+        cur_min = ops_count[i][1]
+        a,b,c = math.inf, math.inf, math.inf
 
-    for i in range(0, len(opsCount)):
+        prev_val1 = ops_count[i-1][1]
+        a = min( prev_val1 + 1, cur_min )
 
-        a = opsCount[i-1] + 1
-        b = i/3 if i % 3 == 0 else math.inf
-        c = i/2 if i % 2 == 0 else math.inf
-
-        if b != math.inf: 
-            b = opsCount[int(b)] + 1
+        if (cur_val % 2 == 0): 
+            prev_val2 = ops_count[(int(cur_val/2) - 1)][1]
+            b = min( prev_val2 + 1, cur_min )
         
-        if c != math.inf: 
-            c = opsCount[int(c)] + 1
-        
-        opsCount[i] =  min(a, b, c)
-    
-    return opsCount
+        if (cur_val % 3 == 0):
+            prev_val3 = ops_count[(int(cur_val/3) - 1)][1]
+            c = min( prev_val3 + 1, cur_min )
 
-# if __name__ == '__main__':
-#     n = int(input)
-#     sequence = list(optimal_sequence(n))
-#     print(len(sequence) - 1)
-#     for x in sequence:
-#         print(x, end=' ')
+        ops_count[i][1] = min(cur_val, a, b, c)
 
-print(optimal_sequence(5))
+        # Need to dynamically generate path 
+        if (a == min(cur_val, a, b, c)):
+            ops_count[i][2] = i-1
+        elif (b == min(cur_val, a, b, c)):
+            ops_count[i][2] = (int(cur_val/2) - 1)
+        elif (c == min(cur_val, a, b, c)):
+            ops_count[i][2] = (int(cur_val/3) - 1)
+
+    cur_idx = len(ops_count) - 1
+    path = []
+
+    while cur_idx != 0: 
+        path.append(ops_count[cur_idx][0])
+        cur_idx = ops_count[cur_idx][2]
+
+    path.append(1)
+    path.reverse()
+    return path
+
+if __name__ == '__main__':
+    n = int(input())
+    sequence = list(optimal_sequence(n))
+    print(len(sequence) - 1)
+    for x in sequence:
+        print(x, end=' ')
