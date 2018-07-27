83508957c67eacdba1fdb59607ab082754ac627a
jtamsut <jtamsut1993@gmail.com>
2018-07-27 13:23:14 -0700

    implement knapsack without reps

--- algorithmic-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
+++ algorithmic-toolbox/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.py
@@ -1,13 +1,31 @@
 # Uses python3
 import sys
 
-def optimal_weight(W, w):
-    # write your code here
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+def optimal_weight(W, weights):
+
+    n = len(weights)
+    values = []
+    for _ in range(0, n+1):
+        new_row = [] 
+        for _ in range(0, W+1): 
+            new_row.append(0)
+        values.append(new_row) 
+
+    for i in range(1, n+1):
+        for w in range(1, W+1):
+            cur_weight = weights[i-1]
+            val_no_i = values[i-1][w]
+
+            if cur_weight <= w: 
+                rem_item_sol = values[i-1][w-cur_weight] + cur_weight
+                if rem_item_sol >= val_no_i: 
+                    values[i][w] = rem_item_sol
+                else: 
+                    values[i][w] = val_no_i
+            else: 
+                values[i][w] = values[i-1][w]
+
+    return values[n][W]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
